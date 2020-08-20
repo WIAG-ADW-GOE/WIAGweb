ea68ad48f56eb0633286e66f82fdc0e7159d3bad
Georg Hertkorn <ghertko@gwdg.de>
2020-08-20 16:30:02 +0200

    Initial files for homepage and about

---
+++ src/Controller/Start.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class Start extends AbstractController {
+    /**
+     * @Route("/", name="welcome")
+     */
+	public function welcome() {
+        return $this->render('start/welcome.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about-wiag")
+     */
+	public function about() {
+        return $this->render('start/about.html.twig');
+    }
+
+    
+}
