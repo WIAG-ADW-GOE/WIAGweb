4a63cb56da23b0f16a8119670322864040863b48
Georg Hertkorn <ghertko@gwdg.de>
2020-12-07 12:53:42 +0100

    add contact info

--- src/Controller/Start.php
+++ src/Controller/Start.php
@@ -8,31 +8,37 @@ class Start extends AbstractController {
     /**
      * @Route("/", name="wiag_welcome")
      */
-	public function welcome() {
+    public function welcome() {
         return $this->render('start/welcome.html.twig');
     }
 
     /**
      * @Route("/about", name="wiag_about")
      */
-	public function about() {
+    public function about() {
         return $this->render('start/about.html.twig');
     }
 
     /**
      * @Route("/api-doc", name="api_documentation")
      */
-	public function apidoc() {
+    public function apidoc() {
         return $this->render('start/apidoc.html.twig');
     }
 
+    /**
+     * @Route("/contact", name="wiag_contact")
+     */
+    public function contact() {
+        return $this->render('start/contact.html.twig');
+    }
 
     /**
      * @Route("/phpinfo", name="wiag_phpinfo")
      */
-	public function show_phpinfo() {
+    public function show_phpinfo() {
         phpinfo();
     }
 
-    
+
 }
